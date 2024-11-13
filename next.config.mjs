4d45c149fb2f18c2ca8d269c3bb545e52799359d
harshit2608 <harshitrock111@gmail.com>
2024-11-14 00:10:19 +0530

    Fix deployment images issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProductionEnv = process.env.NODE_ENV == 'production';
+
 const nextConfig = {
+  basePath: isProductionEnv ? '/frontend-showcase' : '',
   output: 'export'
 };
 
